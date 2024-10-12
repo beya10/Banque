14109961da9d224d48f5835d97edead2341b4d98
Beya Jean-Marie <beya.mulumba@wetestify>
2024-10-12 11:42:02 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Person{
+
+    public string FirstName;
+    public string LastName;
+    public DateTime BirthDate;
+
+    
+}
